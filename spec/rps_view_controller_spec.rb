c9f270c4ef5342ce565b0c2069d378f4bb578935
Tim Snowhite <tim@snowhitesolutions.com>
2014-01-10 10:39:49 -0500

    [prototype] Rock Paper Scissors view controller spec.

---
+++ spec/rps_view_controller_spec.rb
@@ -0,0 +1,25 @@
+describe "The RPS view controller" do
+  tests RpsController
+  setup do
+    controller.game_ai = :deterministic_game
+  end
+  it "has a Rock label" do
+    view('Rock').should.not == nil
+  end
+
+  it "starts a game" do
+    tap 'Rock'
+    controller.game.isValid.should == true
+  end
+
+  it "updates both players" do
+    player = view('Your Move:')
+    computer = view('Their Move:')
+    outcome = view('Winner:')
+    tap 'Rock'
+    player.text.should == 'Rock'
+    computer.text.should == 'Paper'
+    outcome.text.should == 'Opponent'
+  end
+
+end
