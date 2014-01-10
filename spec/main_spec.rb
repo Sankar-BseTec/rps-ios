5e42d61216575d6bc1518de81cde2bf7a19afd64
Tim Snowhite <tim@snowhitesolutions.com>
2014-01-10 12:00:00 -0500

    [prototype specs]  A quick sanity test to keep this TDD

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -6,4 +6,7 @@ describe "Application 'RPS'" do
   it "has one window" do
     @app.windows.size.should == 1
   end
+  it "loads RpsController" do
+    RpsController.should === @app.windows.first.rootViewController
+  end
 end
